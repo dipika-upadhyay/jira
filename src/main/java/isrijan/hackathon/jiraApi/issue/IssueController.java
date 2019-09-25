51db74c194cb85c9d4415c832043141a9cf53da8
dipika-upadhyay <34876104+dipika-upadhyay@users.noreply.github.com>
2019-09-25 14:46:24 +0530

    Add files via upload

--- src/main/java/isrijan/hackathon/jiraApi/issue/IssueController.java
+++ src/main/java/isrijan/hackathon/jiraApi/issue/IssueController.java
@@ -1,5 +1,6 @@
 package isrijan.hackathon.jiraApi.issue;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,6 +10,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+
+
 
 @RestController
 public class IssueController {
@@ -29,5 +38,26 @@ public class IssueController {
 	@RequestMapping(method=RequestMethod.POST, value="/issues")
 	public void addIssue(@RequestBody Object object) {
 		System.out.println("Request triggered");
+		try {
+		OkHttpClient client = new OkHttpClient();
+
+		MediaType mediaType = MediaType.parse("application/json");
+		com.squareup.okhttp.RequestBody body = com.squareup.okhttp.RequestBody.create(mediaType, "{\n  \"accountId\": \"5d846b7d01e2cb0c3020019a\"\n}");
+		Request request = new Request.Builder()
+		  .url("https://jirawithdocumentation.atlassian.net/rest/api/3/issue/DB-3/assignee")
+		  .put(body)
+		  .addHeader("Content-Type", "application/json")
+		  .addHeader("Authorization", "Basic ZGhhcmFzbWlsZTE1QGdtYWlsLmNvbTpaZ2c2eFNncG9iTE82MXdycUt5RUJEOUY=")
+		  .addHeader("Cache-Control", "no-cache")
+		  .addHeader("Postman-Token", "2aebdd48-c4d6-4d13-8fbe-4ef12146cb99")
+		  .build();
+			System.out.println("Request prepared"+request.toString());
+		
+		   Response response = client.newCall(request).execute();
+		} catch (IOException e) {
+			System.out.println("exceptionL::::");
+			e.printStackTrace();
+			
+		}
 	}
 }
